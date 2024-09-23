896a543d6d76ebc2e8771a0e37b628e411a3d817
wesleyalanbenne <alan.bennemann@escola.pr.gov.br>
2024-09-23 10:19:34 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {//configuraçao
+
+createCanvas(1000,1000);//tamanho da tela
+
+background("black");// cor de fundo
+
+}
+
+function draw() {//desenhar
+
+stroke("red");//cor de contorno
+
+triangle(100, 200,150,400,400,400);//tamanho do triangulo
+
+}
